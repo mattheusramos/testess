d1ef9009322130d00eff7f0c081809084538b187
Matheus Ramos <132701491+mattheusramos@users.noreply.github.com>
2024-09-03 09:09:17 -0300

    testes de exercicio

---
+++ exerc1.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Digite o valor de A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Digite o valor de B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Digite o valor de C: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int soma = A + B;
+System.Console.WriteLine(soma);
+
+if(soma < C){
+    System.Console.WriteLine("A soma de A + B é menor que C!");
+}else{
+    System.Console.WriteLine("A soma dos valores é maior que C!");
+}
